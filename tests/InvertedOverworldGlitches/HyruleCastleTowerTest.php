6e2708086c19756ecfc3b8616f466915f45708be
Michael Kunze <erzengelmichael@live.de>
2019-11-29 17:42:27 -0500

    More Tests
    Describing Tests: They Test

--- tests/InvertedOverworldGlitches/HyruleCastleTowerTest.php
+++ tests/InvertedOverworldGlitches/HyruleCastleTowerTest.php
@@ -25,17 +25,63 @@ class HyruleCastleTowerTest extends TestCase
         unset($this->world);
     }
 
-    // Entry
-    public function testCapeOrUpgradedSwordRequiredToEnter()
+    /**
+     * @param bool $access
+     * @param array $items
+     * @param array $except
+     *
+     * @dataProvider entryPool
+     */
+    public function testEntry(bool $access, array $items, array $except = [])
     {
-        $this->assertFalse($this->world->getRegion('Hyrule Castle Tower')
-            ->canEnter($this->world->getLocations(), $this->allItemsExcept(['Cape', 'UpgradedSword'])));
+        if (count($except)) {
+            $this->collected = $this->allItemsExcept($except);
+        }
+
+        $this->addCollected($items);
+
+        $this->assertEquals($access, $this->world->getRegion('Hyrule Castle Tower')
+            ->canEnter($this->world->getLocations(), $this->collected));
     }
 
-    // Completion
-    public function testSwordRequiredToComplete()
+    public function entryPool()
     {
-        $this->assertFalse($this->world->getRegion('Hyrule Castle Tower')
-            ->canComplete($this->world->getLocations(), $this->allItemsExcept(['AnySword'])));
+        return [
+            [false, []],
+            [true, ['PegasusBoots']],
+        ];
+    }
+
+    /**
+     * @param bool $access
+     * @param array $items
+     * @param array $except
+     *
+     * @dataProvider completePool
+     */
+    public function testComplete(bool $access, array $items, array $except = [])
+    {
+        if (count($except)) {
+            $this->collected = $this->allItemsExcept($except);
+        }
+
+        $this->addCollected($items);
+
+        $this->assertEquals($access, $this->world->getRegion('Hyrule Castle Tower')
+            ->canComplete($this->world->getLocations(), $this->collected));
+    }
+
+    public function completePool()
+    {
+        return [
+            [false, []],
+            [false, [], ['Lamp']],
+            [false, [], ['AnySword']],
+            [true, ['KeyA1', 'KeyA1', 'PegasusBoots', 'Lamp', 'UncleSword']],
+            [true, ['KeyA1', 'KeyA1', 'PegasusBoots', 'Lamp', 'ProgressiveSword']],
+            [true, ['KeyA1', 'KeyA1', 'PegasusBoots', 'Lamp', 'MasterSword']],
+            [true, ['KeyA1', 'KeyA1', 'PegasusBoots', 'Lamp', 'L3Sword']],
+            [true, ['KeyA1', 'KeyA1', 'PegasusBoots', 'Lamp', 'L4Sword']],
+        ];
     }
 }
